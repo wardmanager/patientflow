fa505c1b6644824086d05b83605e681c3fd4b81b
Daniel Rosenthal <dev@wardmanager.com>
2016-05-16 12:13:26 -0400

    fixed routing for get

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :ads
+  resources :ads do
+    #get 'list' => 'ads#list2'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'ads#index'
+  root 'ads#create'
+
+  #get 'ads/list' => 'ads#list', as: :adlist
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
 
   # Example resource route with options:
   #   resources :products do
